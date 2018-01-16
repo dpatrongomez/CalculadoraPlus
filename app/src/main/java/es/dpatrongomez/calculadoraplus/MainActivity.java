54836943927cdcf95f6f71c246e7fdad6793d291
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-01-17 00:04:47 +0100

    Add Menu

--- app/src/main/java/es/dpatrongomez/calculadoraplus/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/calculadoraplus/MainActivity.java
@@ -72,17 +72,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.estandar) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.cientifica) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.programador) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.fecha) {
 
         }
 
